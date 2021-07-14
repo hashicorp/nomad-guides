45d603bb466fc94c3c5319007e0ae6caf0329d19
raylaijh <32132864+raylaijh@users.noreply.github.com>
2021-07-14 12:48:23 +0800

    Update outputs.tf

--- application-deployment/consul-connect-with-nomad/aws/outputs.tf
+++ application-deployment/consul-connect-with-nomad/aws/outputs.tf
@@ -1,15 +1,15 @@
 output "IP_Addresses" {
   value = <<CONFIGURATION
 
-Client public IPs: ${join(", ", module.nomadconsul.client_public_ips)}
-Client private IPs: ${join(", ", module.nomadconsul.client_private_ips)}
-Server public IPs: ${join(", ", module.nomadconsul.primary_server_public_ips)}
-Server private IPs: ${join(", ", module.nomadconsul.primary_server_private_ips)}
+Client public IPs: ${join(", ", module.nomadconsul.client_public_ips[0])}
+Client private IPs: ${join(", ", module.nomadconsul.client_private_ips[0])}
+Server public IPs: ${join(", ", module.nomadconsul.primary_server_public_ips[0])}
+Server private IPs: ${join(", ", module.nomadconsul.primary_server_private_ips[0])}
 
-`ssh -i "${var.key_name}.pem" ubuntu@$(module.nomadconsul.primary_server_public_ips)`
+`ssh -i "${var.key_name}.pem" ubuntu@$(module.nomadconsul.primary_server_public_ips[0])`
 
-The Consul UI can be accessed at http://$(module.nomadconsul.primary_server_public_ips):8500/ui
-The Nomad UI can be accessed at http://$(module.nomadconsul.primary_server_public_ips):4646/ui
+The Consul UI can be accessed at http://$(module.nomadconsul.primary_server_public_ips[0]):8500/ui
+The Nomad UI can be accessed at http://$(module.nomadconsul.primary_server_public_ips[0]):4646/ui
 
 CONFIGURATION
 }
