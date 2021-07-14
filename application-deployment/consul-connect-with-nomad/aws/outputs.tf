ae61a1cd8980ccad6ec3ad86bc77327ab2e33bb0
raylaijh <32132864+raylaijh@users.noreply.github.com>
2021-07-14 12:39:00 +0800

    Update outputs.tf

--- application-deployment/consul-connect-with-nomad/aws/outputs.tf
+++ application-deployment/consul-connect-with-nomad/aws/outputs.tf
@@ -6,10 +6,10 @@ Client private IPs: ${join(", ", module.nomadconsul.client_private_ips)}
 Server public IPs: ${join(", ", module.nomadconsul.primary_server_public_ips)}
 Server private IPs: ${join(", ", module.nomadconsul.primary_server_private_ips)}
 
-`ssh -i "${var.key_name}.pem" ubuntu@${element(module.nomadconsul.primary_server_public_ips, 0)}`
+`ssh -i "${var.key_name}.pem" ubuntu@$(module.nomadconsul.primary_server_public_ips)`
 
-The Consul UI can be accessed at http://${element(module.nomadconsul.primary_server_public_ips, 0)}:8500/ui
-The Nomad UI can be accessed at http://${element(module.nomadconsul.primary_server_public_ips, 0)}:4646/ui
+The Consul UI can be accessed at http://$(module.nomadconsul.primary_server_public_ips):8500/ui
+The Nomad UI can be accessed at http://$(module.nomadconsul.primary_server_public_ips):4646/ui
 
 CONFIGURATION
 }
